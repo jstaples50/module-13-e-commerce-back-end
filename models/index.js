aef7202dda206701362215c38503ad7cd1269483
Jared Staples <jstaplesmusic@gmail.com>
2022-12-09 14:32:38 -0500

    Created modules, and routes for category, product, and tags

--- models/index.js
+++ models/index.js
@@ -1,16 +1,38 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignkey: "category",
+});
+
 // Categories have many Products
+
+Category.hasMany(Product, {
+  foreignkey: "product",
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    as: "product_tag",
+  },
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    as: "product_tag",
+  },
+});
 
 module.exports = {
   Product,
